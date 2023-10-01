201c8715d9de09df0243eb5c8e169a640ddcf555
Vanessa Brito <vanbrigo@Air-de-Vanessa.home>
2023-10-01 16:35:26 +0200

    perf: array named intento save colors picked by player

--- js/main.js
+++ js/main.js
@@ -3,7 +3,9 @@ const circulo1=document.getElementById('circulo1')
 const circulo2=document.getElementById('circulo2')
 const circulo3=document.getElementById('circulo3')
 const circulo4=document.getElementById('circulo4')
+const botonCheck=document.getElementById('botonCheck')
 let posicionColor=0
+const intento=[]
 
 const combinacionAleatoria = (() => {
     const combinacion = []
@@ -19,24 +21,30 @@ const combinacionAleatoria = (() => {
 const cambioColores =(()=>{
     let colorElegido= colores[posicionColor]
     posicionColor=(posicionColor+1)%colores.length
-    console.log(posicionColor)
-    console.log(colorElegido)
     return colorElegido
 })
-
 document.addEventListener('click',(evento)=>{
     if (evento.target.id==='circulo1'){
-        let colorElegido=cambioColores()
-        circulo1.style.background=colorElegido
+        colorElegido1=cambioColores()
+        circulo1.style.background=colorElegido1
     } if (evento.target.id==='circulo2'){
-        let colorElegido=cambioColores()
-        circulo2.style.background=colorElegido
+        colorElegido2=cambioColores()
+        circulo2.style.background=colorElegido2
     } if(evento.target.id==='circulo3'){
-        let colorElegido=cambioColores()
-        circulo3.style.background=colorElegido
+        colorElegido3=cambioColores()
+        circulo3.style.background=colorElegido3
     } if(evento.target.id==='circulo4'){
-        let colorElegido=cambioColores()
-        circulo4.style.background=colorElegido
+        colorElegido4=cambioColores()
+        circulo4.style.background=colorElegido4
+    } if(evento.target.id==='botonCheck'){
+        intento.push(colorElegido1,colorElegido2,colorElegido3,colorElegido4)
+        console.log(intento)
     }
 })
+
+// const verificar=((combinacion,intento)=>{
+//  if(combinacion===intento){
+
+//  } if(combinacion!=intento && )
+// })
 
