5bcc0c5887d92ff023d70e1e9281fe14765c5cfc
Vanessa Brito <vanbrigo@Air-de-Vanessa.home>
2023-10-01 14:02:32 +0200

    perf: new fuction to pick the color in each circle

--- js/main.js
+++ js/main.js
@@ -5,6 +5,12 @@ const color4 =document.getElementById('colorCuatro')
 const color5 =document.getElementById('colorCinco')
 const color6 =document.getElementById('colorSeis')
 const colors =[color1,color2,color3,color4,color5,color6]
+const colores=['#56ff50','#ff1493','#8a2be2','#ffd700','#ff0000','#00b7ff']
+const circulo1=document.getElementById('circulo1')
+const circulo2=document.getElementById('circulo2')
+const circulo3=document.getElementById('circulo3')
+const circulo4=document.getElementById('circulo4')
+let posicionColor=0
 
 const combinacionAleatoria = (() => {
     const combinacion = []
@@ -17,9 +23,28 @@ const combinacionAleatoria = (() => {
     console.log(combinacion)
     return combinacion
 })
-
-const seleccionColorCirculo=(()=>{
-    
-
+const cambioColores =(()=>{
+    let colorElegido= colores[posicionColor]
+    posicionColor=(posicionColor+1)%colores.length
+    console.log(posicionColor)
+    console.log(colorElegido)
+    return colorElegido
 })
+
+document.addEventListener('click',(evento)=>{
+    if (evento.target.id==='circulo1'){
+        let colorElegido=cambioColores()
+        circulo1.style.background=colorElegido
+    } if (evento.target.id==='circulo2'){
+        let colorElegido=cambioColores()
+        circulo2.style.background=colorElegido
+    } if(evento.target.id==='circulo3'){
+        let colorElegido=cambioColores()
+        circulo3.style.background=colorElegido
+    } if(evento.target.id==='circulo4'){
+        let colorElegido=cambioColores()
+        circulo4.style.background=colorElegido
+    }
+})
+
 
