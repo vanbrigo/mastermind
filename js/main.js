ac9e63d58685a37d7adf382b9a72bb688632b16b
Vanessa Brito <vanbrigo@Air-de-Vanessa.home>
2023-10-01 18:32:08 +0200

    feat: new condition to not to change color decision after player press check button

--- js/main.js
+++ js/main.js
@@ -5,46 +5,52 @@ const circulo3=document.getElementById('circulo3')
 const circulo4=document.getElementById('circulo4')
 const botonCheck=document.getElementById('botonCheck')
 let posicionColor=0
-const intento=[]
+const intento1=[]
+const combinacion = []
 
 const combinacionAleatoria = (() => {
-    const combinacion = []
     for (i = 0; i <= 3; i++) {
         const numeroAleatorio = Math.floor(Math.random() * 6)
         console.log(numeroAleatorio)
         const colorSeleccionado = colores[[numeroAleatorio]]
         combinacion.push(colorSeleccionado)
     }
-    console.log(combinacion)
     return combinacion
 })
+
 const cambioColores =(()=>{
     let colorElegido= colores[posicionColor]
     posicionColor=(posicionColor+1)%colores.length
     return colorElegido
 })
+
 document.addEventListener('click',(evento)=>{
-    if (evento.target.id==='circulo1'){
+    if (evento.target.id==='circulo1' && intento1<=0){
         colorElegido1=cambioColores()
         circulo1.style.background=colorElegido1
-    } if (evento.target.id==='circulo2'){
+    } if (evento.target.id==='circulo2' && intento1<=0){
         colorElegido2=cambioColores()
         circulo2.style.background=colorElegido2
-    } if(evento.target.id==='circulo3'){
+    } if(evento.target.id==='circulo3' && intento1<=0){
         colorElegido3=cambioColores()
         circulo3.style.background=colorElegido3
-    } if(evento.target.id==='circulo4'){
+    } if(evento.target.id==='circulo4' && intento1<=0){
         colorElegido4=cambioColores()
         circulo4.style.background=colorElegido4
-    } if(evento.target.id==='botonCheck'){
-        intento.push(colorElegido1,colorElegido2,colorElegido3,colorElegido4)
-        console.log(intento)
+    } if(evento.target.id==='botonCheck' && intento1.length<=4){
+        intento1.push(colorElegido1,colorElegido2,colorElegido3,colorElegido4)
+        console.log(intento1.length)
     }
 })
+console.log(combinacionAleatoria())
 
-// const verificar=((combinacion,intento)=>{
-//  if(combinacion===intento){
+const verificar=(() => {
+  if (combinacion === intento1) {
+    console.log('ganaste')
+} if (combinacion[posicionColor] === intento1[posicionColor]) {
+ console.log('blue')
+}
+})
 
-//  } if(combinacion!=intento && )
-// })
+
 
