ab8b49c56b263b4dd805e753f95bc1396f6bc2a0
Vanessa Brito <vanbrigo@MacBook-Air-de-Vanessa.local>
2023-10-10 14:07:04 +0200

    fix: added boolean to not click twice in the same color when player is picking colors to play

--- js/main.js
+++ js/main.js
@@ -17,14 +17,14 @@ options.forEach(Element=>{
         let g = parseInt(valoresRGB[1]);
         let b = parseInt(valoresRGB[2]);
         let colorHexadecimal = "#" + ((1 << 24) | (r << 16) | (g << 8) | b).toString(16).slice(1);
-        document.addEventListener('click',(eleccion)=>{
+        Element.addEventListener('click',(eleccion)=>{
             if(eleccion.target.id===Element.id && coloresElegidosUsuario.length<=numeroMaximo){
                 coloresElegidosUsuario.push(colorHexadecimal) 
                 for(let i=0;i<circles.length;i++){
                     circles[i].style.backgroundColor=coloresElegidosUsuario[i]
                 }
             } 
-        }) 
+        },{once:true}) 
       }    
 })
 botonSave.addEventListener('click',()=>{
@@ -61,43 +61,4 @@ botonNext.addEventListener('click',()=>{
     }
     })
 
-
-
-
-// botonNext.addEventListener('click',()=>{
-//     if(player.value.length>=3){
-//     localStorage.setItem('player',player.value)
-//     } else{
-        
-//     }
-// })
-
-// document.addEventListener('click',(eleccion)=>{
-//     if(eleccion.target.id==='color1' && coloresElegidosUsuario.length<=numeroMaximo){
-//         coloresElegidosUsuario.push('#56ff50')
-//     } if(eleccion.target.id==='color2' && coloresElegidosUsuario.length<=numeroMaximo){
-//         coloresElegidosUsuario.push('#ff1493')
-//     } if(eleccion.target.id==='color3' && coloresElegidosUsuario.length<=numeroMaximo){
-//         coloresElegidosUsuario.push('#8a2be2')
-//     } if(eleccion.target.id==='color4' && coloresElegidosUsuario.length<=numeroMaximo){
-//         coloresElegidosUsuario.push('#ffd700')
-//     } if(eleccion.target.id==='color5' && coloresElegidosUsuario.length<=numeroMaximo){
-//         coloresElegidosUsuario.push('#ff0000')
-//     } if(eleccion.target.id==='color6' && coloresElegidosUsuario.length<=numeroMaximo){
-//         coloresElegidosUsuario.push('#00b7ff')
-//     } if(eleccion.target.id==='color7' && coloresElegidosUsuario.length<=numeroMaximo){
-//         coloresElegidosUsuario.push('#ff8c00')
-//     } if(eleccion.target.id==='color8' && coloresElegidosUsuario.length<=numeroMaximo){
-//         coloresElegidosUsuario.push('#3442d9')
-//     } if(eleccion.target.id==='color9' && coloresElegidosUsuario.length<=numeroMaximo){
-//         coloresElegidosUsuario.push('#d96ccc')
-//     } if(eleccion.target.id==='color10' && coloresElegidosUsuario.length<=numeroMaximo){
-//         coloresElegidosUsuario.push('#48e0cb')
-//     } if(eleccion.target.id==='botonSave' && coloresElegidosUsuario.length==6){
-//         localStorage.setItem('colores',JSON.stringify(coloresElegidosUsuario))
-//         console.log(coloresElegidosUsuario.length)
-//     } if(eleccion.target.id==='botonNext'){
-        
-//     }
-// })
 
